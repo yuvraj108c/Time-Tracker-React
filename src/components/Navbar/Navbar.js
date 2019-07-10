8102f8ce6fe213a203dc89237beb2792e5ca0128
yuvraj108c <yuvraj.seegolam@umail.uom.ac.mu>
2019-07-10 14:43:28 +0400

    Added navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Menu, Container } from "semantic-ui-react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <Menu size="massive" fixed="top" inverted>
+        <Container>
+          <Menu.Item as={Link} to="/" header>
+            Time Tracker
+          </Menu.Item>
+          <Menu.Menu position="right">
+            <Menu.Item as={Link} to="/">
+              Track
+            </Menu.Item>
+            <Menu.Item as={Link} to="/analyze">
+              Analyze
+            </Menu.Item>
+          </Menu.Menu>
+        </Container>
+      </Menu>
+    );
+  }
+}
+
+export default Navbar;
