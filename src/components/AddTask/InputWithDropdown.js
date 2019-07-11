20647d055549037459aee76e04c8bed6358e9e37
yuvraj108c <yuvraj.seegolam@umail.uom.ac.mu>
2019-07-11 14:40:11 +0400

    Added props

--- src/components/AddTask/InputWithDropdown.js
+++ src/components/AddTask/InputWithDropdown.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
-import { Dropdown, Input } from "semantic-ui-react";
+import { Select, Input } from "semantic-ui-react";
 
 class InputWithDropdown extends Component {
   render() {
     return (
       <Input
+        name="taskName"
+        value={this.props.taskName}
+        onChange={this.props.handleInputChange}
         action={
-          <Dropdown
-            button
-            basic
-            floating
+          <Select
+            compact
             options={this.props.options}
-            defaultValue={this.props.defaultCategory}
+            name="taskCategory"
+            value={this.props.taskCategory}
+            onChange={this.props.handleDropdownChange}
           />
         }
         placeholder="Enter task"
