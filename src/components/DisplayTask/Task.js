c60c1cd156fe689d82bc0a528e95ff31ee7b6c04
yuvraj108c <yuvraj.seegolam@umail.uom.ac.mu>
2019-07-12 15:59:46 +0400

    Created task item component

---
+++ src/components/DisplayTask/Task.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { Item, Grid, Label } from "semantic-ui-react";
+
+import "./style.scss";
+
+class Task extends Component {
+  render() {
+    const {
+      name,
+      categoryName,
+      categoryColor,
+      startTime,
+      endTime,
+      duration
+    } = this.props;
+    return (
+      <Item className="task-item">
+        <Item.Content>
+          <Grid>
+            <Grid.Column mobile={6}>{name}</Grid.Column>
+            <Grid.Column mobile={3} style={{ color: categoryColor }}>
+              <Label
+                circular
+                empty
+                style={{ backgroundColor: categoryColor }}
+              />
+              {categoryName}
+            </Grid.Column>
+            <Grid.Column mobile={2}>{startTime}</Grid.Column>
+            <Grid.Column mobile={2}>{endTime}</Grid.Column>
+            <Grid.Column mobile={3}>{duration}</Grid.Column>
+          </Grid>
+        </Item.Content>
+      </Item>
+    );
+  }
+}
+
+export default Task;
