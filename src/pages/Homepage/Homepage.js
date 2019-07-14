281e9a3b87de79cc953b26cc67cc586b4c9e7b3c
yuvraj108c <yuvraj108c@gmail.com>
2019-07-14 17:34:55 +0400

    Created homepage container

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,25 +1,32 @@
 import React, { Component } from "react";
 import getFromServer from "../../utils/getFromServer";
+import { Container, Grid, Loader, Dimmer } from "semantic-ui-react";
+
+import AddCategory from "../../components/AddCategory";
+import AddTask from "../../components/AddTask";
+import DisplayTask from "../../components/DisplayTask";
 
 class Homepage extends Component {
   constructor() {
     super();
     this.state = {
       tasks: [],
-      categories: []
+      categories: [],
+      loading: true
     };
     this.fetchCategories = this.fetchCategories.bind(this);
     this.fetchTasks = this.fetchTasks.bind(this);
   }
+
   componentDidMount() {
     this.fetchCategories();
     this.fetchTasks();
   }
 
   fetchCategories() {
-    //   Get all categories
-    getFromServer(process.env.REACT_APP_GET_CATEGORIES_URL).then(c => {
-      this.setState({ categories: c });
+    // Create object {name,color} from category
+    getFromServer(process.env.REACT_APP_GET_CATEGORIES_URL).then(categories => {
+      this.setState({ categories });
     });
   }
 
@@ -27,11 +34,38 @@ class Homepage extends Component {
     //   Get all tasks
     getFromServer(process.env.REACT_APP_GET_TASKS_URL).then(t => {
       this.setState({ tasks: t });
+      this.setState({ loading: false });
     });
   }
 
   render() {
-    return <div />;
+    const { tasks, categories, loading } = this.state;
+    return (
+      <div className="homepage">
+        <div className="main-content">
+          {loading === true ? (
+            <Dimmer active inverted>
+              <Loader inverted>Loading</Loader>
+            </Dimmer>
+          ) : (
+            <Container>
+              <Grid>
+                <Grid.Column mobile={16} computer={10}>
+                  <AddTask
+                    categories={categories}
+                    fetchTasks={this.fetchTasks}
+                  />
+                  <DisplayTask tasks={tasks} />
+                </Grid.Column>
+                <Grid.Column mobile={16} computer={6}>
+                  <AddCategory fetchCategories={this.fetchCategories} />
+                </Grid.Column>
+              </Grid>
+            </Container>
+          )}
+        </div>
+      </div>
+    );
   }
 }
 
