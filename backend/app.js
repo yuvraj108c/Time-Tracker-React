01a62875035d4ceeba5e1cd622768ddf7162ef0c
yuvraj108c <yuvraj.seegolam@umail.uom.ac.mu>
2019-07-29 01:43:16 -0700

    Fixed backend

--- backend/app.js
+++ backend/app.js
@@ -1,50 +1,21 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
+const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-var indexRouter = require("./routes/index");
-var categoriesRouter = require("./routes/categories");
-var tasksRouter = require("./routes/tasks");
-
-var app = express();
+const app = express();
+const PORT = 4000;
 
 // Db connection
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true }, err => {
   console.log(err ? err : `Connected to MongoDb`);
 });
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/categories", categoriesRouter);
-app.use("/tasks", tasksRouter);
+app.use("/categories",  require("./routes/categories"));
+app.use("/tasks", require("./routes/tasks"));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-module.exports = app;
+app.listen(PORT, err => {
+  console.log(err ? err : `Server running on port ${PORT}`);
+})
